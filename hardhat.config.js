1522bf233b705e4781f3919577b22883272dfa4f
Vi <gimmefear@gmail.com>
2022-08-06 14:27:20 +0300

    allow unlimited size

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.5.12",
+  networks: {
+    localhost: {
+      allowUnlimitedContractSize: true,
+    },
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+  }
 };
